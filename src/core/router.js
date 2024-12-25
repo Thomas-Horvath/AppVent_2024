57cbbedd092a69261b65afacf55ce1ba023ee610
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-12-25 17:00:26 +0100

    Day 12, config.js

--- src/core/router.js
+++ src/core/router.js
@@ -1,6 +1,16 @@
+
 class Router {
+    defaultPath = '';
+    pathDelimiter = '';
 
     routes = {};
+
+    constructor(config) {
+        this.defaultPath = config.defaultPath;
+        this.pathDelimiter = config.pathDelimiter;
+
+        window.addEventListener('popstate', () => this.manage())  // a visszanyil kattintásra ugrik egyet vissza
+    }
 
     register(path, method) {
         this.routes[path] = method;
@@ -9,9 +19,24 @@ class Router {
 
 
     navigate(path) {
-        this.routes[path]();
+        history.pushState(null, null, `${this.pathDelimiter}${path}`); // itt íródik az urlbe a href érték
+        this.manage();
+        // this.routes[path]();
     }
 
+
+    manage() {
+        let path = this.#getPath();
+        this.routes[path]();
+    }
+ 
+
+    #getPath() {
+        let path =  location.search || location.hash || location.pathname;
+        path = path.substring(1);
+        if (!path || path === 'index.html') path = this.defaultPath;
+        return path;
+    }
 }
 
 export default Router;
--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 import "./components/nav-bar.js";
 import Router from './core/router.js';
+import config from './config.js';
 
 
-
+ 
 // router
 
-let router = new Router();
+let router = new Router(config);
 
 router.register('lists', () => { console.log('Lists..') })
     .register('family', () => { console.log('Family..') })
     .register('ideas', () => { console.log('Ideas..') })
     .register('home', () => { console.log('Home..') })
+    .manage();
 
 
 
